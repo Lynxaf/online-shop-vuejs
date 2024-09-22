2023f93a54137dc24d152a5d3dd0b1e73951ffc5
Mukhlishah Afdhaliyah <116530302+lisaamir@users.noreply.github.com>
2024-09-22 21:08:17 +0800

    Online Shop Website
    An online shop website that can be set in price ranges according to the buyer's wishes, built using HTML, CSS, JavaScript, and the Bootstrap and Vue Js frameworks

---
+++ app.js
@@ -0,0 +1,195 @@
+// var product = {
+//     maximum : 100,
+//     products: 
+//     [
+//         {
+//             "id": "429",
+//             "name": "Red Leather Handbag",
+//             "price": "75.99",
+//             "image_title": "red-leather-handbag",
+//             "image": "https://cdn.pixabay.com/photo/2016/11/23/18/12/bag-1854148_1280.jpg",
+//             "description": "A chic red leather handbag, perfect for adding a pop of color to any outfit."
+//         },
+//         {
+//             "id": "430",
+//             "name": "Black Quilted Shoulder Bag",
+//             "price": "85.00",
+//             "image_title": "black-quilted-shoulder-bag",
+//             "image": "https://images.pexels.com/photos/1667070/pexels-photo-1667070.jpeg",
+//             "description": "A classic black quilted shoulder bag with gold chain detailing, ideal for evening outings."
+//         },
+//         {
+//             "id": "431",
+//             "name": "Brown Tote Bag",
+//             "price": "65.50",
+//             "image_title": "brown-tote-bag",
+//             "image": "https://images.pexels.com/photos/1350524/pexels-photo-1350524.jpeg",
+//             "description": "A spacious brown tote bag, perfect for everyday use and carrying essentials."
+//         },
+//         {
+//             "id": "432",
+//             "name": "Beige Crossbody Bag",
+//             "price": "49.99",
+//             "image_title": "beige-crossbody-bag",
+//             "image": "https://images.pexels.com/photos/1667076/pexels-photo-1667076.jpeg",
+//             "description": "A trendy beige crossbody bag, perfect for casual and hands-free styling."
+//         },
+//         {
+//             "id": "433",
+//             "name": "Gray Satchel Bag",
+//             "price": "70.00",
+//             "image_title": "gray-satchel-bag",
+//             "image": "https://images.pexels.com/photos/2053220/pexels-photo-2053220.jpeg",
+//             "description": "A sophisticated gray satchel bag, ideal for both work and weekend wear."
+//         },
+//         {
+//             "id": "434",
+//             "name": "White Bucket Bag",
+//             "price": "55.00",
+//             "image_title": "white-bucket-bag",
+//             "image": "https://images.pexels.com/photos/1488463/pexels-photo-1488463.jpeg",
+//             "description": "A stylish white bucket bag, offering a modern and trendy look for any occasion."
+//         },
+//         {
+//             "id": "435",
+//             "name": "Pink Mini Bag",
+//             "price": "45.99",
+//             "image_title": "pink-mini-bag",
+//             "image": "https://media.istockphoto.com/id/1346494580/id/foto/tas-wanita-kulit-merah-muda-dengan-pegangan-dengan-latar-belakang-merah-muda-konsep.jpg?s=612x612&w=is&k=20&c=U9H-3li1-5OdexaXgopjNBiS_jCWPtALrZJ91SCyqqs=",
+//             "description": "A cute and compact pink mini bag, perfect for a night out or special occasions."
+//         },
+//         {
+//             "id": "436",
+//             "name": "Green Handheld Bag",
+//             "price": "60.00",
+//             "image_title": "green-handheld-bag",
+//             "image": "https://plus.unsplash.com/premium_photo-1723549679986-16e018db1105?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+//             "description": "A bold green handheld bag, a perfect accessory to make a statement."
+//         },
+//         {
+//             "id": "437",
+//             "name": "Yellow Clutch Bag",
+//             "price": "40.00",
+//             "image_title": "yellow-clutch-bag",
+//             "image": "https://media.istockphoto.com/id/484565414/id/foto/dompet-kulit-kuning.jpg?s=612x612&w=is&k=20&c=6zgmFUn3rpZyC85svdjV6PZqs1OsXOgkstbQ0m--F9s=",
+//             "description": "A vibrant yellow clutch bag, great for evening wear and adding a pop of color."
+//         },
+//         {
+//             "id": "438",
+//             "name": "Navy Blue Backpack",
+//             "price": "55.99",
+//             "image_title": "navy-blue-backpack",
+//             "image": "https://cdn.pixabay.com/photo/2016/11/19/14/56/backpack-1839705_1280.jpg",
+//             "description": "A functional navy blue backpack, perfect for casual outings and travel."
+//         },
+//         {
+//             "id": "439",
+//             "name": "Brown Leather Messenger Bag",
+//             "price": "85.99",
+//             "image_title": "brown-leather-messenger-bag",
+//             "image": "https://images.pexels.com/photos/2081199/pexels-photo-2081199.jpeg",
+//             "description": "A stylish brown leather messenger bag, offering a blend of fashion and functionality."
+//         },
+//         {
+//             "id": "440",
+//             "name": "Silver Evening Clutch",
+//             "price": "50.00",
+//             "image_title": "silver-evening-clutch",
+//             "image": "https://media.istockphoto.com/id/1257711987/id/foto/wanita-muda-menunjukkan-tas-kopling-berkilau-perak-di-latar-belakang-merah-muda-closeup.jpg?s=612x612&w=is&k=20&c=JFSiWAFPM7GdJL-VZbEEI7XiLIzG4vFvSMLrLO5ak64=",
+//             "description": "A glamorous silver evening clutch, ideal for formal events and parties."
+//         },
+//         {
+//             "id": "441",
+//             "name": "Red Crossbody Bag",
+//             "price": "45.00",
+//             "image_title": "red-crossbody-bag",
+//             "image": "https://images.pexels.com/photos/298864/pexels-photo-298864.jpeg",
+//             "description": "A sleek and compact red crossbody bag, perfect for everyday chic."
+//         },
+//         {
+//             "id": "442",
+//             "name": "Black Leather Hobo Bag",
+//             "price": "95.00",
+//             "image_title": "black-leather-hobo-bag",
+//             "image": "https://media.istockphoto.com/id/1161727005/id/foto/seri-aksesoris-mode-wanita-terisolasi-di-latar-belakang-putih.jpg?s=1024x1024&w=is&k=20&c=MgBGMXxcdI4VcZVmAQFVRoTkchn1ohmZyXCR9nv5PbU=",
+//             "description": "A luxurious black leather hobo bag, great for both casual and formal wear."
+//         },
+//         {
+//             "id": "443",
+//             "name": "Purple Tote Bag",
+//             "price": "65.00",
+//             "image_title": "purple-tote-bag",
+//             "image": "https://images.pexels.com/photos/1804149/pexels-photo-1804149.jpeg",
+//             "description": "A vibrant purple tote bag, adding a bold and unique flair to your style."
+//         },
+//         {
+//             "id": "444",
+//             "name": "Nude Handbag",
+//             "price": "58.50",
+//             "image_title": "nude-handbag",
+//             "image": "https://images.pexels.com/photos/601170/pexels-photo-601170.jpeg",
+//             "description": "A neutral-toned nude handbag, perfect for pairing with any outfit."
+//         },
+//         {
+//             "id": "445",
+//             "name": "Gold Chain Shoulder Bag",
+//             "price": "78.00",
+//             "image_title": "gold-chain-shoulder-bag",
+//             "image": "https://images.pexels.com/photos/698465/pexels-photo-698465.jpeg",
+//             "description": "An elegant shoulder bag with a gold chain strap, perfect for evening wear."
+//         },
+//         {
+//             "id": "446",
+//             "name": "Tan Bucket Bag",
+//             "price": "52.99",
+//             "image_title": "tan-bucket-bag",
+//             "image": "https://media.istockphoto.com/id/2153634301/id/foto/tas-ember-kulit-cokelat-elegan-dengan-tali-serut.jpg?s=612x612&w=is&k=20&c=Z05ch0EzcEScEQ5USFvpW-W3Jd89llO_JPKqegJbsSQ=",
+//             "description": "A casual tan bucket bag, ideal for daily use and combining fashion with function."
+//         },
+//         {
+//             "id": "447",
+//             "name": "Brown Suede Backpack",
+//             "price": "80.00",
+//             "image_title": "brown-suede-backpack",
+//             "image": "https://media.istockphoto.com/id/1205382232/id/foto/aksesoris-pria-sepatu-unta-ransel-khaki-dan-sabuk-kulit.jpg?s=612x612&w=is&k=20&c=HHI0L1aZpayCWlzh-45i_nYnmBRYGX4lJL6_pXP7lwU=",
+//             "description": "A soft brown suede backpack, a stylish choice for casual outings."
+//         },
+//         {
+//             "id": "448",
+//             "name": "White Designer Handbag",
+//             "price": "120.00",
+//             "image_title": "white-designer-handbag",
+//             "image": "https://images.pexels.com/photos/267320/pexels-photo-267320.jpeg",
+//             "description": "A luxurious white designer handbag, perfect for high-end fashion lovers."
+//         }
+//     ]
+    
+    
+// }
+
+
+var app  = new Vue({
+    el: '#app',
+    data : {
+        maximum : 50,
+        products: null,
+        cart: [],
+        style:{
+            label: ['font-weight-bold','mr-2'],
+            inputWidth : 60,
+            sliderStatus : false
+        }
+    },
+    mounted: function() {
+          fetch('https://hplussport.com/api/products/order/price')
+          .then(response => response.json())
+          .then(data =>{
+            this.products = data;
+          })
+    },
+    computed: {
+        sliderState: function (){
+            return this.style.sliderStatus ? 'd-flex' : 'd-none'
+        }
+    },
+});
